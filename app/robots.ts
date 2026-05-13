a2dd327dead5de926172f4e22dfe9a82f89d74f6
Indranil Sharma <indranilsharma371@gmail.com>
2026-05-13 15:55:59 +0530

    Added sitemap and robots SEO

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://globalalpha.co.in/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://globalalpha.co.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://globalalpha.co.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://globalalpha.co.in/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://globalalpha.co.in/markets",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://globalalpha.co.in/youtube",
+      lastModified: new Date(),
+    },
+  ];
+}
